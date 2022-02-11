adb7700da2b2ef69805a0aa6ba2f04f91cb68334
Rendy Arya Kemal <renrror@gmail.com>
2022-02-11 09:42:02 +0700

    Stick text and add extra padding on projects

--- src/components/works/layout.tsx
+++ src/components/works/layout.tsx
@@ -18,24 +18,26 @@ const ProjectLayout: FunctionComponent<LayoutProps> = ({
       id={id}
       className={
         'p-8 md:px-16 2xl:px-64 3xl:px-96 md:py-8 ' +
-        'md:flex md:flex-row md:space-x-5 ' +
+        'md:flex md:flex-row md:space-x-5 md:items-stretch ' +
         (alternate ? 'bg-gray-800' : 'bg-gray-200')
       }
     >
-      <motion.h2
-        initial={{ opacity: 0, y: 25 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true }}
-        className={
-          'font-montserrat font-bold text-4xl ' +
-          'text-center md:text-right pb-8 md:w-1/5 flex-none ' +
-          (alternate ? 'text-white' : 'text-black')
-        }
-      >
-        {title}
-      </motion.h2>
+      <div className="w-full relative pb-8 md:w-1/5 flex-none">
+        <motion.h2
+          initial={{ opacity: 0, y: 25 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+          className={
+            'sticky top-5 font-montserrat font-bold text-4xl ' +
+            'text-center md:text-right ' +
+            (alternate ? 'text-white' : 'text-black')
+          }
+        >
+          {title}
+        </motion.h2>
+      </div>
       <div className="hidden md:block md:w-1 rounded-full bg-gray-500" />
-      <div className="flex flex-col space-y-2 grow">{children}</div>
+      <div className="flex flex-col space-y-4 grow">{children}</div>
     </div>
   );
 };
